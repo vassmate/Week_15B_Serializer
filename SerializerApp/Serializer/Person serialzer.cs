5df3ad7925775024fbb7f38aec395b8015b9d215
vassmate <vassmate08@gmail.com>
2016-05-12 13:28:23 +0200

    Adding initial task files
    Windows form design is prepared

---
+++ SerializerApp/Serializer/Person serialzer.cs
@@ -0,0 +1,12 @@
+﻿using System.Windows.Forms;
+
+namespace Serializer
+{
+    public partial class PersonSerialzer : Form
+    {
+        public PersonSerialzer()
+        {
+            InitializeComponent();
+        }
+    }
+}
