c228b41094f472031907db77dbdcdde814544ac5
vassmate <vassmate08@gmail.com>
2016-05-14 13:18:48 +0200

    Deserialization and stepping are working
    Also the form is now showing the serialization DateTime and the current
    person.

---
+++ SerializerApp/Serializer/PersonSerialzer.Designer.cs
@@ -0,0 +1,203 @@
+﻿namespace Serializer
+{
+    partial class PersonSerialzer
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.txtName = new System.Windows.Forms.TextBox();
+            this.txtAddress = new System.Windows.Forms.TextBox();
+            this.txtPhone = new System.Windows.Forms.TextBox();
+            this.lblName = new System.Windows.Forms.Label();
+            this.lblAddress = new System.Windows.Forms.Label();
+            this.lblPhone = new System.Windows.Forms.Label();
+            this.btnPrev = new System.Windows.Forms.Button();
+            this.btnNext = new System.Windows.Forms.Button();
+            this.btnSave = new System.Windows.Forms.Button();
+            this.lblPersonId = new System.Windows.Forms.Label();
+            this.txtPersonId = new System.Windows.Forms.TextBox();
+            this.lblSerialDate = new System.Windows.Forms.Label();
+            this.txtSerialDate = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // txtName
+            // 
+            this.txtName.Location = new System.Drawing.Point(29, 31);
+            this.txtName.Name = "txtName";
+            this.txtName.Size = new System.Drawing.Size(343, 20);
+            this.txtName.TabIndex = 0;
+            // 
+            // txtAddress
+            // 
+            this.txtAddress.Location = new System.Drawing.Point(29, 81);
+            this.txtAddress.Name = "txtAddress";
+            this.txtAddress.Size = new System.Drawing.Size(343, 20);
+            this.txtAddress.TabIndex = 1;
+            // 
+            // txtPhone
+            // 
+            this.txtPhone.Location = new System.Drawing.Point(29, 131);
+            this.txtPhone.Name = "txtPhone";
+            this.txtPhone.Size = new System.Drawing.Size(343, 20);
+            this.txtPhone.TabIndex = 2;
+            // 
+            // lblName
+            // 
+            this.lblName.AutoSize = true;
+            this.lblName.Location = new System.Drawing.Point(26, 15);
+            this.lblName.Name = "lblName";
+            this.lblName.Size = new System.Drawing.Size(35, 13);
+            this.lblName.TabIndex = 3;
+            this.lblName.Text = "Name";
+            // 
+            // lblAddress
+            // 
+            this.lblAddress.AutoSize = true;
+            this.lblAddress.Location = new System.Drawing.Point(26, 65);
+            this.lblAddress.Name = "lblAddress";
+            this.lblAddress.Size = new System.Drawing.Size(45, 13);
+            this.lblAddress.TabIndex = 4;
+            this.lblAddress.Text = "Address";
+            // 
+            // lblPhone
+            // 
+            this.lblPhone.AutoSize = true;
+            this.lblPhone.Location = new System.Drawing.Point(26, 115);
+            this.lblPhone.Name = "lblPhone";
+            this.lblPhone.Size = new System.Drawing.Size(38, 13);
+            this.lblPhone.TabIndex = 5;
+            this.lblPhone.Text = "Phone";
+            // 
+            // btnPrev
+            // 
+            this.btnPrev.Location = new System.Drawing.Point(29, 226);
+            this.btnPrev.Name = "btnPrev";
+            this.btnPrev.Size = new System.Drawing.Size(75, 23);
+            this.btnPrev.TabIndex = 6;
+            this.btnPrev.Text = "Previous";
+            this.btnPrev.UseVisualStyleBackColor = true;
+            this.btnPrev.Click += new System.EventHandler(this.btnPrev_Click);
+            // 
+            // btnNext
+            // 
+            this.btnNext.Location = new System.Drawing.Point(124, 226);
+            this.btnNext.Name = "btnNext";
+            this.btnNext.Size = new System.Drawing.Size(75, 23);
+            this.btnNext.TabIndex = 7;
+            this.btnNext.Text = "Next";
+            this.btnNext.UseVisualStyleBackColor = true;
+            this.btnNext.Click += new System.EventHandler(this.btnNext_Click);
+            // 
+            // btnSave
+            // 
+            this.btnSave.Location = new System.Drawing.Point(297, 226);
+            this.btnSave.Name = "btnSave";
+            this.btnSave.Size = new System.Drawing.Size(75, 23);
+            this.btnSave.TabIndex = 8;
+            this.btnSave.Text = "Save";
+            this.btnSave.UseVisualStyleBackColor = true;
+            this.btnSave.Click += new System.EventHandler(this.btnSave_Click);
+            // 
+            // lblPersonId
+            // 
+            this.lblPersonId.AutoSize = true;
+            this.lblPersonId.Location = new System.Drawing.Point(26, 180);
+            this.lblPersonId.Name = "lblPersonId";
+            this.lblPersonId.Size = new System.Drawing.Size(79, 13);
+            this.lblPersonId.TabIndex = 9;
+            this.lblPersonId.Text = "Current person:";
+            // 
+            // txtPersonId
+            // 
+            this.txtPersonId.Location = new System.Drawing.Point(108, 177);
+            this.txtPersonId.Name = "txtPersonId";
+            this.txtPersonId.ReadOnly = true;
+            this.txtPersonId.Size = new System.Drawing.Size(18, 20);
+            this.txtPersonId.TabIndex = 10;
+            // 
+            // lblSerialDate
+            // 
+            this.lblSerialDate.AutoSize = true;
+            this.lblSerialDate.Location = new System.Drawing.Point(139, 180);
+            this.lblSerialDate.Name = "lblSerialDate";
+            this.lblSerialDate.Size = new System.Drawing.Size(63, 13);
+            this.lblSerialDate.TabIndex = 11;
+            this.lblSerialDate.Text = "Serial. date:";
+            // 
+            // txtSerialDate
+            // 
+            this.txtSerialDate.Location = new System.Drawing.Point(205, 177);
+            this.txtSerialDate.Name = "txtSerialDate";
+            this.txtSerialDate.ReadOnly = true;
+            this.txtSerialDate.Size = new System.Drawing.Size(112, 20);
+            this.txtSerialDate.TabIndex = 12;
+            // 
+            // PersonSerialzer
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(384, 261);
+            this.Controls.Add(this.txtSerialDate);
+            this.Controls.Add(this.lblSerialDate);
+            this.Controls.Add(this.txtPersonId);
+            this.Controls.Add(this.lblPersonId);
+            this.Controls.Add(this.btnSave);
+            this.Controls.Add(this.btnNext);
+            this.Controls.Add(this.btnPrev);
+            this.Controls.Add(this.lblPhone);
+            this.Controls.Add(this.lblAddress);
+            this.Controls.Add(this.lblName);
+            this.Controls.Add(this.txtPhone);
+            this.Controls.Add(this.txtAddress);
+            this.Controls.Add(this.txtName);
+            this.MaximumSize = new System.Drawing.Size(400, 300);
+            this.MinimumSize = new System.Drawing.Size(400, 300);
+            this.Name = "PersonSerialzer";
+            this.Text = "PersonSerialzer";
+            this.Load += new System.EventHandler(this.PersonSerialzer_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox txtName;
+        private System.Windows.Forms.TextBox txtAddress;
+        private System.Windows.Forms.TextBox txtPhone;
+        private System.Windows.Forms.Label lblName;
+        private System.Windows.Forms.Label lblAddress;
+        private System.Windows.Forms.Label lblPhone;
+        private System.Windows.Forms.Button btnPrev;
+        private System.Windows.Forms.Button btnNext;
+        private System.Windows.Forms.Button btnSave;
+        private System.Windows.Forms.Label lblPersonId;
+        private System.Windows.Forms.TextBox txtPersonId;
+        private System.Windows.Forms.Label lblSerialDate;
+        private System.Windows.Forms.TextBox txtSerialDate;
+    }
+}
+
---
+++ SerializerApp/Serializer/PersonSerialzer.cs
@@ -0,0 +1,132 @@
+﻿using System.IO;
+using System.Runtime.Serialization;
+using System.Runtime.Serialization.Formatters.Binary;
+using System.Windows.Forms;
+
+namespace Serializer
+{
+    public partial class PersonSerialzer : Form
+    {
+        private int _fileNumber = 1;
+        private int _fileListStepper;
+        private readonly string _workingFolderPath = @"C:\serialized_persons";
+        private string _fileName = "person01.dat";
+        private readonly IFormatter _binaryFormatter = new BinaryFormatter();
+        private string[] _fileList;
+
+        public PersonSerialzer()
+        {
+            InitializeComponent();
+        }
+
+        private void PersonSerialzer_Load(object sender, System.EventArgs e)
+        {
+            string msgBoxTitle = "Directory info";
+            string noDirectoryMsg = "Directory created at:\n" + _workingFolderPath;
+            string noFilesMsg = "There aren't any files to show in the folder:\n" + _workingFolderPath;
+
+            if (!Directory.Exists(_workingFolderPath))
+            {
+                Directory.CreateDirectory(_workingFolderPath);
+                MessageBox.Show(noDirectoryMsg, msgBoxTitle, MessageBoxButtons.OK, MessageBoxIcon.Information);
+            }
+            else
+            {
+                _fileList = Directory.GetFiles(_workingFolderPath);
+
+                if (_fileList.Length == 0)
+                {
+                    MessageBox.Show(noFilesMsg, msgBoxTitle, MessageBoxButtons.OK, MessageBoxIcon.Information);
+                }
+                else
+                {
+                    Person personFromFile = Deserialize(_binaryFormatter, _workingFolderPath + @"\" + _fileName);
+                    SetTextFieldValues(personFromFile);
+                    _fileNumber += _fileList.Length;
+                }
+            }
+        }
+
+        private void btnSave_Click(object sender, System.EventArgs e)
+        {
+            string serialInfoTitle = "Serialization info";
+            string serialWarnTitle = "Warning!";
+            string serialWarnMsg = "Fields can't be empty!";
+
+            if (!txtName.Text.Equals("") && !txtName.Text.Equals(" ") &&
+                !txtAddress.Text.Equals("") && !txtAddress.Text.Equals(" ") &&
+                !txtPhone.Text.Equals("") && !txtPhone.Text.Equals(" ") &&
+                _fileList.Length < 100)
+            {
+                SetSerializationValues();
+                MessageBox.Show($"Data serialized to:\n{_workingFolderPath}\\{_fileName}!",
+                    serialInfoTitle, MessageBoxButtons.OK, MessageBoxIcon.Information);
+            }
+            else
+            {
+                MessageBox.Show(serialWarnMsg, serialWarnTitle, MessageBoxButtons.OK, MessageBoxIcon.Warning);
+            }
+        }
+
+        private void btnPrev_Click(object sender, System.EventArgs e)
+        {
+            if (_fileListStepper > 0)
+            {
+                _fileListStepper--;
+                Person personFromFile = Deserialize(_binaryFormatter, _fileList[_fileListStepper]);
+                SetTextFieldValues(personFromFile);
+            }
+        }
+
+        private void btnNext_Click(object sender, System.EventArgs e)
+        {
+            if (_fileListStepper < _fileList.Length - 1)
+            {
+                _fileListStepper++;
+                Person personFromFile = Deserialize(_binaryFormatter, _fileList[_fileListStepper]);
+                SetTextFieldValues(personFromFile);
+            }
+        }
+
+        private void Serialze(Person person, IFormatter iformatter)
+        {
+            _fileName = _fileNumber < 10 ? $"person{0}{_fileNumber}.dat" : $"person{_fileNumber}.dat";
+
+            FileStream fileStream = new FileStream(_workingFolderPath + @"\" + _fileName, FileMode.Create);
+            iformatter.Serialize(fileStream, person);
+            fileStream.Close();
+        }
+
+        private Person Deserialize(IFormatter iformatter, string filePath)
+        {
+            FileStream fileStream = new FileStream(filePath, FileMode.Open);
+            Person personFromFile = (Person)iformatter.Deserialize(fileStream);
+            fileStream.Close();
+
+            return personFromFile;
+        }
+
+        private void SetTextFieldValues(Person personFromFile)
+        {
+            txtName.Text = personFromFile.Name;
+            txtAddress.Text = personFromFile.Address;
+            txtPhone.Text = personFromFile.Phone;
+
+            txtPersonId.Text = _fileListStepper.ToString();
+            txtSerialDate.Text = personFromFile.DateOfRecording.ToString();
+        }
+
+        private void SetSerializationValues()
+        {
+            Person newPerson = new Person(txtName.Text, txtAddress.Text, txtPhone.Text);
+            Serialze(newPerson, _binaryFormatter);
+
+            _fileList = Directory.GetFiles(_workingFolderPath);
+            _fileListStepper = _fileList.Length - 1;
+            _fileNumber++;
+
+            txtPersonId.Text = _fileListStepper.ToString();
+            txtSerialDate.Text = newPerson.DateOfRecording.ToString();
+        }
+    }
+}
